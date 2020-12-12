f4177c2456b1be544c7cc5064da16b08df7dcf25
Paula Quitián <pqhernandez@gmail.com>
2020-12-12 20:53:15 +0100

    add Login page and User profile

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from 'react-redux';
 
 // Contexts
 import { DataProvider } from './contexts/Data';
+
+// Store
+import store from '../store';
 
 // Views
 import Albums from './views/Albums';
 import Home from './views/Home';
 import NotFound from './views/NotFound';
 import AlbumDetail from './views/AlbumDetail';
+import UserProfile from './views/UserProfile';
+import Login from './views/Login';
+
+// Other components 
+import PrivateRoute  from './components/PrivateRoute';
 
 // Css
 import './App.css';
@@ -16,18 +25,22 @@ import './App.css';
 const App = (props) => {
 
   return (
+    <Provider store={store}>
     <DataProvider>
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/albumes" exact component={Albums}></Route>
-          <Route path="/album/:id" component={AlbumDetail}></Route>
-          <Route component={NotFound}></Route>
-        </Switch>
-      </div>
-    </Router>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route path="/" exact component={Home}/>
+              <Route path="/albumes" exact component={Albums}/>
+              <Route path="/album/:id" component={AlbumDetail}/>
+              <Route path="/login" exact component={Login}/>
+              <PrivateRoute path="/user" component={UserProfile}/>
+              <Route component={NotFound}/>
+            </Switch>
+          </div>
+        </Router>
     </DataProvider>
+    </Provider>
   );
 
 };
