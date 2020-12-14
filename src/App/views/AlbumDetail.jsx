93c4aa0719a047ddadb78445f94efe1c36500e76
Paula Quitián <pqhernandez@gmail.com>
2020-12-14 14:19:40 +0100

    get albums from store in AlbumDetail

--- src/App/views/AlbumDetail.jsx
+++ src/App/views/AlbumDetail.jsx
@@ -1,15 +1,59 @@
 import React, { useEffect, useContext, useState } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+
+// Actions 
+import { getSongs } from '../../actions/songs';
+import { getAlbums } from '../../actions/albums';
 
 
-const AlbumDetail = ({ match }) => {
-    const albumId = match.params.id;
+const AlbumDetail = (props) => {
+    const albumId = props.match.params.id;
 
-    return (
-        <>
-        <h1>Álbum: {albumId}</h1>
-        {/*  album && album.name */}
+    // Props
+    const { albums, isLoading, getAlbums } = props;
+
+
+    // State
+    const [tracklist, setTracklist] = useState([]);
+    const [album, setAlbum] = useState({});
+
+    const getCurrentAlbum = (albumList) => {
+        return albumList.filter(a => { 
+            return a.id === parseInt(albumId);
+        });
+    };
+
+    useEffect(() => {
+        getAlbums();
+    }, []);
+
+    useEffect(() => {
+        const currentAlbum = getCurrentAlbum(albums);
+        setAlbum(currentAlbum[0]);
+    }, [albums]);
+
+    if (isLoading) {
+        return <><p>Cargando...</p>
+            <h1>Álbum: {albumId}</h1>
         </>
-    );
-}
+    } else {
+        return <>
+        <p>{album && album.name}</p>
+        <p>{JSON.stringify(album)}</p>
+        </>
+    }
+};
 
-export default AlbumDetail;
+const mapStateToProps = (state) => ({...state.albums});
+const mapDispatchToProps = (dispatch) => ({
+    // getSongs: () => dispatch(getSongs()),
+    getAlbums: () => dispatch(getAlbums()),
+});
+/* const mapDispatchToProps = (dispatch) => {
+    return {
+        ...bindActionCreators({getAlbums, getSongs}, dispatch)
+    }
+}; */
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumDetail);
