ae104eaeb7ee7becfeee372110838e28c04b3fef
Paula Quitián <pqhernandez@gmail.com>
2020-12-14 01:42:47 +0100

    display album tracklist

--- src/App/components/AlbumListItem.jsx
+++ src/App/components/AlbumListItem.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-const AlbumListItem = ({ album }) => <>
+// Actions 
+import { getSongs } from '../../actions/songs'
+
+const AlbumListItem = (props) => {
+    const { album, getSongs, isLoading, songs, error } = props;
+
+    const [tracklist, setTracklist] = useState([]);
+
+    const getAlbumTracklist = () => {
+        return songs.reduce((acc, curr) => {
+        
+            if (curr.album_id === album.id) acc.push(curr);
+            return acc;
+        }, []);
+    };
+
+    useEffect(() => {
+        getSongs();
+    }, []);
+
+    useEffect(() => {
+        setTracklist(getAlbumTracklist);
+    }, [songs]);
+
+    return <>
         <h1>{album.name}</h1>
         <p>{album.artist}</p>
         <img src={album.cover} alt={album.name}/>
-    </>;
+        {tracklist && tracklist.map((song, i) => <p key={i}>{song.name}</p>)}
+    </>
+};
 
-export default AlbumListItem;
+const mapStateToProps = (state) => ({...state.songs});
+const mapDispatchToProps = (dispatch) => ({
+    getSongs: () => dispatch(getSongs()),
+}); 
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumListItem);
